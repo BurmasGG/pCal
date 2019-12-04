d8114320ea8bddc6fe3ac9773c7758ef3482314e
Emil Faerch <emil.faerch@live.dk>
2019-12-04 18:08:22 +0100

    Notification WIP - needs "hasNotified" bool in database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { interval } from 'rxjs/observable/interval';
 import { EventViewComponent } from './event-view/event-view.component';
 import { HomeViewComponent } from './home-view/home-view.component';
 import { ReminderService } from './reminder.service';
+import { EventDialogComponent } from './event-dialog/event-dialog.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,14 @@ import { ReminderService } from './reminder.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Emil er trash';
-constructor(private reminderService: ReminderService, public newappointment: AppointmentService, public appointmentcomponent: NewAppointmentComponent, private homeView: HomeViewComponent){}
+  title = 'Emil er fam';
 
-  source = interval(1000);
+  constructor(private reminderService: ReminderService, public newappointment: AppointmentService, 
+    public appointmentcomponent: NewAppointmentComponent, private homeView: HomeViewComponent, private dialog: MatDialog){}
+
+  reminderCheckInterval = 1000; // default: 45(000) (milli)seconds
+  
+  source = interval(this.reminderCheckInterval);
   subscribe = this.source.subscribe(lol => {
     this.ReminderCheck();
   });
@@ -24,45 +30,56 @@ constructor(private reminderService: ReminderService, public newappointment: App
   ReminderCheck()
   {
     this.reminderService.GetTodayDate();
-    console.log("Checking " + this.reminderService.eventsCurWeek.length + " Event Reminders")
+    console.log("Checking " + this.reminderService.eventsCurWeek.length + " Days for Event Reminders")
 
     let s_hour = Number(this.reminderService.dp.transform(this.reminderService.date, 'HH'));
     let s_minutes = this.reminderService.dp.transform(this.reminderService.date, 'mm');
 
-    // current time
     let curTime = s_hour + ":" + s_minutes;
 
-    // current time - 1 hour
-    let notifyTime = (Number(s_hour - 1)).toString() + ":" + s_minutes;
+    // current time
 
-    for (let day = 0; day < this.reminderService.eventsCurWeek.length; day++) {
-      this.reminderService.eventsCurWeek[day].forEach(event => {
-        if (event.day == this.reminderService.curDay)
+    for (let day = 0; day < this.reminderService.eventsCurWeek.length; day++) { // go through each day's ... 
+    console.log("Day " + day + "...")
+      this.reminderService.eventsCurWeek[day].forEach(event => {                // ... event(s)
+        console.log("[" + day + "] " + event.note + ", " + event.day + " (" + this.reminderService.curDay + ")")
+        if (event.day == this.reminderService.curDay) // is event today?
         {
-          if (event.time == notifyTime)
+          let notifyTime = (Number(event.time.split(':')[0] - 1)).toString() + ":" + s_minutes; // eventTime - 1 hour
+          if (curTime == notifyTime) // is event time an hour from now?
           {
-            this.Notify(event);
-          }
-        }
-        else
-        {
-          if (event.time == curTime)
-          {
-            this.Remind(event);
+            this.Notify(event); // notify
           }
         }
       });
     }
 }
 
-  Remind(e)
-  {
-    console.log("Minder dig lige om " + e.note + ", d. " + e.day + " kl. " + e.time)
-  }
-
   Notify(e)
-  {
-    console.log("Begivenhed om en time: " + e.note)
-  }
+  {    
+    console.log("NOTIFYING");
+    let dialogRef = this.dialog.open(EventDialogComponent, {
+      autoFocus: true,
+      disableClose: false,
 
+      data: {
+        isNoti: true,
+        id: e._id,
+        type: e.type,
+        note: e.note,
+        year: e.year,
+        month: e.month,
+        day: e.day,
+        time: e.time,
+        people: e.people,
+        place: e.place
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(_result => {
+      const dialogResult = dialogRef.componentInstance.dialogResult;
+
+      
+    });
+  }
 }
