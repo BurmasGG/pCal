8f85228532b4b59e92afbd7f626a8a842e4211bd
Burmas <guitartard@hotmail.com>
2019-12-04 16:45:43 +0100

    Emil er ikke trash
    JK

--- src/app/event-dialog/event-dialog.component.ts
+++ src/app/event-dialog/event-dialog.component.ts
@@ -10,14 +10,12 @@ import { AppointmentService } from '../newAppointment.service';
   styleUrls: ['./event-dialog.component.css']
 })
 
+
 export class EventDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private eventservice: AppointmentService,) {
   }
 
-  // Looking for customization?
-  // For default customization of the window go to app.module.ts and look under 'providers'
-
-  // for content customization go to event-dialog.component.css or .html
+  isNoti = true;
 
   public dialogResult;
 
