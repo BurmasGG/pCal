8bc7cb43982c1f6bc79ff1016cef2e1a1d1776a5
Faelled <cfalle16@student.aau.dk>
2019-12-06 11:29:13 +0100

    Forgot something to flowingleds

--- MongoBackend/server.js
+++ MongoBackend/server.js
@@ -15,8 +15,8 @@ LED27 = new Gpio(27, 'out'),
 LED22 = new Gpio(22, 'out'),
 LED18 = new Gpio(18, 'out'); // use Gpio on pin 4 and specify that is is output
 var leds = [LED04, LED17, LED27, LED22, LED18];*/
-//var indexCount = 0;
-//dir = "up";
+var indexCount;
+dir;
 var LED04,
 LED17,
 LED27,
