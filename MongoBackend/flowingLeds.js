ad8e3475d8a13db5f7d4b158ef85ee61cead669c
Faelled <cfalle16@student.aau.dk>
2019-12-09 18:58:08 +0100

    Update to flowingleds

--- MongoBackend/flowingLeds.js
+++ MongoBackend/flowingLeds.js
@@ -1,12 +1,12 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED04 = new Gpio(4, 'out'), //use declare variables for all the GPIO output pins
+var LED23 = new Gpio(23, 'out'), //use declare variables for all the GPIO output pins
   LED17 = new Gpio(17, 'out'),
   LED27 = new Gpio(27, 'out'),
   LED22 = new Gpio(22, 'out'),
   LED18 = new Gpio(18, 'out');
 
 //Put all the LED variables in an array
-var leds = [LED04, LED17, LED27, LED22, LED18];
+var leds = [LED023, LED17, LED27, LED22, LED18];
 var indexCount = 0; //a counter
 dir = "up"; //variable for flowing direction
 
